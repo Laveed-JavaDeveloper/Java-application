f18680ed04b96dcd653e22307bcd86dc145588cb
Laveed-JavaDeveloper <laveedshaik786@gmail.com>
2025-09-10 22:42:38 +0530

    Code Written

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class helloWorld
+{
+    public void display()
+    {
+        System.out.println("HELLO WORLD PROGRAM");
+    }
+}
+class work
+{
+    public static void main(String[] args) {
+        helloWorld hw=new helloWorld();
+        hw.display();
+    }
+}
